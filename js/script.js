a692b9f59e3a5b41347b4e18a577c99aceb3fedf
Chloe Jones <112436912+chlooeanne@users.noreply.github.com>
2024-04-01 15:45:25 -0400

    updated js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,12 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {get_sticky()};
+// code to make the navbar sticky on scroll:
+window.onscroll = function() {
+  sticky()
+};
 
-// Get the navbar
 var navbar = document.querySelector("nav");
-var h1 = document.querySelector("h1");
-
-// Get the offset position of the navbar
 var sticky = navbar.offsetTop;
 
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function get_sticky() {
+function sticky() {
   if (window.scrollY >= sticky + 5) {
     navbar.classList.add("sticky")
   }
